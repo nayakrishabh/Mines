fbc2ed96a102325b71d0637b3ed20e7c8edb77d2
Rishabh Dev Nayak <rdnayak26@gmail.com>
2024-09-21 14:58:12 +0530

    UiConnector and UiPanel Added

--- Assets/Scripts/PanelCreator.cs
+++ Assets/Scripts/PanelCreator.cs
@@ -5,11 +5,13 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using TMPro;
 
-public class SquarePanelCreator : MonoBehaviour {
+public class PanelCreator : MonoBehaviour {
     [SerializeField]
     private RectTransform canvasTransform; // Assign your Canvas' RectTransform in the Inspector
+    [SerializeField]
+    private GameObject UIPanel;
     
-    public static SquarePanelCreator instance;
+    public static PanelCreator instance;
     //private int tileCount = 16;
     public static GameObject panelObject;
     public static GameObject rightPanel;
@@ -26,15 +28,15 @@ public class SquarePanelCreator : MonoBehaviour {
         }
     }
     void Start() {
-        PanelCreator();
+        PanelCreators();
         CreateRightPanel();
         Tiles.instance.startbuttoncoroutine(panelObject);
-        UiManager.instance.createBLocks(rightPanel);
+        //UiManager.instance.createBLocks(rightPanel);
     }
 
     
 
-    void PanelCreator() {
+    void PanelCreators() {
         // Create a new GameObject with a RectTransform and Image component
         panelObject = new GameObject("SquarePanel", typeof(RectTransform), typeof(Image));
 
@@ -68,12 +70,13 @@ public class SquarePanelCreator : MonoBehaviour {
         rightPanel = new GameObject("RightPanel", typeof(RectTransform), typeof(Image));
         rightPanel.GetComponent<Image>().color = panelcolor;
 
-        verticalLayoutGroup = rightPanel.AddComponent<VerticalLayoutGroup>();
+        //verticalLayoutGroup = rightPanel.AddComponent<VerticalLayoutGroup>();
 
         // Set the panel's parent to be the Canvas
         RectTransform rightPanelRect = rightPanel.GetComponent<RectTransform>();
         rightPanelRect.SetParent(canvasTransform, false);
 
+        Instantiate(UIPanel, rightPanelRect);
         // Set the size of the right panel (same as the original panel)
         rightPanelRect.sizeDelta = Vector2.zero;
 
@@ -85,7 +88,7 @@ public class SquarePanelCreator : MonoBehaviour {
         // Offset the position to move it to the right of the original panel
         rightPanelRect.anchoredPosition = Vector2.zero;
 
-        configVerticalLayoutGroup();
+        //configVerticalLayoutGroup();
     }
 
     #region LayoutConfigurations
@@ -105,20 +108,23 @@ public class SquarePanelCreator : MonoBehaviour {
 
     }
 
-    void configVerticalLayoutGroup() {
-        verticalLayoutGroup.padding = new RectOffset(0,0,0,0);
+    //void configVerticalLayoutGroup() {
+    //    verticalLayoutGroup.padding = new RectOffset(0,0,0,0);
 
-        verticalLayoutGroup.spacing = -88f;
-        verticalLayoutGroup.childAlignment= TextAnchor.MiddleCenter;
-        verticalLayoutGroup.childControlHeight = false;
-        verticalLayoutGroup.childControlWidth = false;
-        verticalLayoutGroup.childScaleHeight = true;
-        verticalLayoutGroup.childScaleWidth = true;
-    }
+    //    verticalLayoutGroup.spacing = -88f;
+    //    verticalLayoutGroup.childAlignment= TextAnchor.MiddleCenter;
+    //    verticalLayoutGroup.childControlHeight = false;
+    //    verticalLayoutGroup.childControlWidth = false;
+    //    verticalLayoutGroup.childScaleHeight = true;
+    //    verticalLayoutGroup.childScaleWidth = true;
+    //}
 
     #endregion
     private void Update() {
 
+    }
+    public GameObject getUiPanel() {
+        return UIPanel;
     }
 }
 
--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -1,97 +1,159 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UiManager : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject containers;
-
-    private int balance;
 
     public static UiManager instance;
-    private List<GameObject> containersList;
 
+    private int balance;
+    private UiConnector connector;
+    private List<GameObject> containersList;
+    private PanelCreator pcRef;
+    private GameObject UiPanel;
+    private int betamount;
+    
     private void Awake() {
         if (instance == null) {
             instance = this;
         }
         else {
-            Destroy(gameObject);  // To ensure singleton behavior
+            Destroy(gameObject);
         }
+        pcRef = PanelCreator.instance;
+        UiPanel = pcRef.getUiPanel();
         containersList = new List<GameObject>();
     }
-    public void createBLocks(GameObject rP) {
-        StartCoroutine(createBlocks(rP));
-    }
-
-    IEnumerator createBlocks(GameObject rP) {
-        int count = 6;
-        RectTransform parentTransform = rP.transform as RectTransform;
-
-        for (int i = 0; i < count; i++) {
-            GameObject container = Instantiate(containers, parentTransform);
-            RectTransform rectTransform = container.GetComponent<RectTransform>();
-            rectTransform.sizeDelta = new Vector2(320f, 122f);
-            containersList.Add(container);
+    void Start() {
+        connector = UiPanel.GetComponent<UiConnector>();
+        Debug.Log(connector.name); 
+        if (connector != null) {
+            addlisteners();
         }
-        blockConfigurator();
-        yield return null;
+        else {
+            Debug.LogError("Failed to find UiConnector component on UiPanel!");
+        }
+    }
+    //public void createBLocks(GameObject rP) {
+    //    StartCoroutine(createBlocks(rP));
+    //}
+    void addlisteners() {
+        connector.increaseButton.onClick.AddListener(increaseBal);
+        connector.decreaseButton.onClick.AddListener(decreaseBal);
+        connector.betButton.onClick.AddListener(applybet);
+        connector.inputField.onValueChanged.AddListener(valuechanged);
+    }
+    void buttonUpdater() {
+    }
+    private void Update() {
+        connector.balance.text = $"Balance : \n {GameManager.instance.getbalance()}";
+        connector.wins.text = $"Balance : \n {GameManager.instance.getbalance()}";
+    }
+    private void valuechanged(string arg0) {
+        betamount = int.Parse(arg0);
     }
 
-    void blockConfigurator() {
-        for (int i = 0; i < containersList.Count; i++) {
-            int index = i;
-            Transform blocktransform = containersList[index].transform;
+    private void applybet() {
+        
+    }
+
+    private void decreaseBal() {
+        betamount--;
+        connector.inputField.text = betamount.ToString();
+        Debug.Log(betamount);
+    }
+
+    private void increaseBal() {
+        betamount++;
+        connector.inputField.text = betamount.ToString();
+    }
+
+    //IEnumerator createBlocks(GameObject rP) {
+    //    int count = 6;
+    //    RectTransform parentTransform = rP.transform as RectTransform;
+
+    //    for (int i = 0; i < count; i++) {
+    //        GameObject container = Instantiate(containers, parentTransform);
+    //        RectTransform rectTransform = container.GetComponent<RectTransform>();
+    //        rectTransform.sizeDelta = new Vector2(320f, 122f);
+    //        containersList.Add(container);
+    //    }
+    //    blockConfigurator();
+    //    yield return null;
+    //}
+
+    //void blockConfigurator() {
+    //    for (int i = 0; i < containersList.Count; i++) {
+    //        int index = i;
+    //        Transform blocktransform = containersList[index].transform;
             
-            switch (i) {
+    //        switch (i) {
 
-                case 0:
-                    textAdder(blocktransform,$"Balance : \n {GameManager.instance.getbalance()}" );
-                    break;
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-                default:
-                    return;
+    //            case 0:
+    //                StartCoroutine(TextAdder(blocktransform, $"Balance : \n {GameManager.instance.getbalance()}"));
+    //                break;
+    //            case 1:
+    //                StartCoroutine(TextAdder(blocktransform, $"Win : \n"));
+    //                break;
+    //            case 2:
+                    
+    //                break;
+    //            case 3:
+    //                break;
+    //            case 4:
+    //                break;
+    //            case 5:
+    //                break;
+    //            default:
+    //                return;
 
-            }
-        }
-    }
+    //        }
+    //    }
+    //}
+    #region BalancePanel Creator
+    //IEnumerator TextAdderCoroutine() {
+    //    StartCoroutine(TextAdder());
+    //    yield return null;
+    //}
+    //IEnumerator TextAdder(Transform blocktransform, string Text) {
+    //    // Create a new TextMeshProUGUI object
+    //    GameObject textObject = new GameObject("Balance", typeof(RectTransform), typeof(TextMeshProUGUI));
 
-    void textAdder(Transform blocktransform, string Text) {
-        // Create a new TextMeshProUGUI object
-        GameObject textObject = new GameObject("Balance", typeof(RectTransform), typeof(TextMeshProUGUI));
+    //    // Set the TextMeshPro object as a child of the Image object
+    //    textObject.transform.SetParent(blocktransform, false); // 'false' to keep the local position
 
-        // Set the TextMeshPro object as a child of the Image object
-        textObject.transform.SetParent(blocktransform, false); // 'false' to keep the local position
+    //    // Configure the RectTransform of the TextMeshPro
+    //    RectTransform textRect = textObject.GetComponent<RectTransform>();
+    //    textRect.pivot = new Vector2(0.5f, 0.5f);
+    //    textRect.anchoredPosition = Vector2.zero;
+    //    textRect.sizeDelta = new Vector2(320f, 100f); // Match the size of the Image
 
-        // Configure the RectTransform of the TextMeshPro
-        RectTransform textRect = textObject.GetComponent<RectTransform>();
-        Debug.Log(textRect.sizeDelta);
-        textRect.pivot = new Vector2(0.5f, 0.5f);
-        textRect.anchoredPosition = Vector2.zero;
-        textRect.sizeDelta = new Vector2(320f, 100f); // Match the size of the Image
+    //    // Configure the TextMeshProUGUI component
+    //    while (true) {
+    //        TextMeshProUGUI tmp = textObject.GetComponent<TextMeshProUGUI>();
+    //        tmp.text = Text;
+    //        tmp.fontStyle = FontStyles.Bold;
+    //        tmp.fontSize = 30;
+    //        tmp.alignment = TextAlignmentOptions.Center;
+    //        tmp.color = Color.black;  // Set text color (example)
 
-        // Configure the TextMeshProUGUI component
-        TextMeshProUGUI tmp = textObject.GetComponent<TextMeshProUGUI>();
-        tmp.text = Text;
-        tmp.fontStyle = FontStyles.Bold;
-        tmp.fontSize = 30;
-        tmp.alignment = TextAlignmentOptions.Center;
-        tmp.color = Color.black;  // Set text color (example)
-    }
+    //        yield return null;
+    //    }
+    //}
+    #endregion
 
-    void buttonMaker(Transform blocktransform, Sprite ui, int index) {
+    void buttonMaker(Transform blocktransform, Sprite ui) {
+        Image buttonIMG = blocktransform.GetComponent<Image>();
+        buttonIMG.sprite = ui;
+        buttonIMG.type = Image.Type.Simple;
+        buttonIMG.preserveAspect = true;
+        buttonIMG.useSpriteMesh = true;
+        buttonIMG.SetNativeSize();
 
     }
 
